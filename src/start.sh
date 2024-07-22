e1d6404cd0c69b0782b5e3e536235662b3a332a0
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2024-07-22 23:37:40 +0800

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -12,7 +12,6 @@ python /stable-diffusion-webui/webui.py \
   --skip-python-version-check \
   --skip-torch-cuda-test \
   --skip-install \
-  --ckpt /model.safetensors \
   --lowram \
   --opt-sdp-attention \
   --disable-safe-unpickle \
